ea9631c535a50ad9959bd600b3a91cb639ea0c78
Joshua Ahimaz <ahimaz.joshua@gmail.com>
2019-02-26 11:57:42 +0800

    Added Basic Server Commands

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const bot = new Discord.Client();
 
-bot.on('message', function(message){
-  if(message.content == PREFIX + 'hey'){
-    message.reply('Hello, how are you?');
+bot.on('message', message => {
+  if(message.content === `${prefix}server`){
+    message.channel.send(`Server Name: ${message.guild.name}\nTotal Members: ${message.guild.memberCount}`);
+  } else if(message.content === `${prefix}owner`){
+    message.reply(`The Owner Of The Server is ${message.author.username}`);
   }
+
 });
 
-bot.once('ready', function(){
-  console.log("Ready For Takeoff!");
+bot.once('ready', () => {
+  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
 })
 
-bot.login(TOKEN);
+bot.login(token);
