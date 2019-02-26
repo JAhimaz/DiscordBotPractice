bf607dcbda1249d913230066127c773002b0ba3b
Joshua Ahimaz <ahimaz.joshua@gmail.com>
2019-02-26 11:27:15 +0800

    Added Basic Bot Setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Testing");
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = 'NTQ5NzkxMTQ4MzI4MjIyNzMy.D1ZBqw.TrNhM-Ji70ijPWMz-iHdt0lGUkU'
+const PREFIX = '!'
+
+bot.on('message', function(message){
+  if(message.content == PREFIX + 'test'){
+    message.reply('Hello, how are you?');
+  }
+});
+
+bot.on('ready', function(){
+  console.log("Ready");
+})
+
+bot.login(TOKEN);
