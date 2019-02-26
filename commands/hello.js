73ce60c9e3ee286b18531f0e349866ebdb83cd14
Joshua Ahimaz <ahimaz.joshua@gmail.com>
2019-02-26 15:37:04 +0800

    Hello Command

---
+++ commands/hello.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'hello',
+	description: 'Say Hello to the wonderful Bot <3',
+  guildOnly: true,
+	execute(message, args) {
+    const helloResponses = ["Hey Cutie :3", "Henlo", "*Tips Fedora* 你好", "Hey Hey Hey", "Uwu Whats Cool"];
+    var response = helloResponses [Math.floor(Math.random()*helloResponses .length)];
+    message.reply(response);
+	},
+};
